1405f39d8ad7c0d07d34865a45d0670092306edb
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-08 20:53:56 -0500

    Changed the option checking and added exit message

--- menus.py
+++ menus.py
@@ -3,6 +3,7 @@ from rich import print
 from rich.table import Table
 import typer
 import cowsay
+import checker
 
 def menu_principal():
 
@@ -11,26 +12,31 @@ def menu_principal():
     print("=" * 50)
 
     table = Table("Comando", "Descripción")
-    table.add_row("1", "Registrar nuevo correo")
+    table.add_row("1", "Registrar nuevo usuario")
     table.add_row("2", "Ver TODOS los correos registrados")
-    table.add_row("3", "Buscar UN correo registrado")
+    table.add_row("3", "Buscar un usuario por su correo")
     table.add_row("4", "Salir de la aplicación")
 
     print(table)
-    print("=" * 20)
+    print("=" * 50)
 
     try:
         while True:
             prompt = typer.prompt("Seleccione una opción [1-4]")
-            if prompt.lower() in ["4"]:
-                if typer.confirm("¿Está seguro de que desea salir?"):
-                    cowsay.ghostbusters("¡Que tengas un maravilloso día!")
-                    raise typer.Exit() # Exit cleanly
-                
-    except Exception as e:
-        print(f"[bold red]An error ocurred: {str(e)}[/bold red]")
+            prompt = checker.check_valid_option(prompt, 1, 4)
+            if not prompt:
+                print("La opcion seleccionada no es valida, vuelve a intentar")
+                continue
+
+            match prompt:
+                case 4:
+                    if typer.confirm("¿Está seguro de que desea salir?"):
+                        cowsay.ghostbusters("¡Que tengas un maravilloso día!")
+                        raise typer.Exit(code = 1) # Exit cleanly
+       
+    except typer.Exit as e:
+        print(f"[bold green]Successfull exit[/bold green]")
 
 
-
-
-
+def create_add_menu():
+    pass
